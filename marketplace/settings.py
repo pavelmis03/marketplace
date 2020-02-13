31c6e3ea21eaaf3a7e3e378fbe527a7643a34c1d
Daniil Konyagin <devdakone22@gmail.com>
2020-02-13 23:18:14 +0300

    Added password reset system

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -129,6 +129,15 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+# LOGIN_URL = '/user/login/'
+# LOGIN_REDIRECT_URL = '/'
+# LOGOUT_REDIRECT_URL = '/'
+
+# EMAIL - для отправки сообщений со сбросом пароля
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shp.ms104.dev.marketplace.bot@gmail.com'
+SERVER_EMAIL = 'shp.ms104.dev.marketplace.bot@gmail.com'
+EMAIL_HOST_PASSWORD = "promprog"
--- user/views.py
+++ user/views.py
@@ -1,13 +1,15 @@
+import os
+
 from django.contrib import auth
 from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 from main.views import get_base_context
 
 
-class LoginView(auth_views.LoginView):
-    template_name = 'login.html'
-    pagename = 'Авторизация'
+class DetailViewContextDefined():
+    pagename = 'unnamed'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -15,6 +17,36 @@ class LoginView(auth_views.LoginView):
         return context
 
 
+class LoginView(auth_views.LoginView, DetailViewContextDefined):
+    pagename = 'Авторизация'
+    template_name = os.path.join('login.html')
+    success_url = 'index'
+
+
 def logout_page(request):
     auth.logout(request)
     return redirect('index')
+
+
+class PasswordResetView(auth_views.PasswordResetView, DetailViewContextDefined):
+    pagename = 'Сброс пароля'
+    template_name = os.path.join('password', 'reset-form.html')
+    email_template_name = os.path.join('password', 'reset-email.html')
+    success_url = reverse_lazy('user.password.reset.done')
+
+
+class PasswordResetCompleteView(auth_views.PasswordResetCompleteView, DetailViewContextDefined):
+    pagename = 'Сброс пароля'
+    template_name = os.path.join('password', 'reset-complete.html')
+
+
+class PasswordResetConfirmView(auth_views.PasswordResetConfirmView, DetailViewContextDefined):
+    pagename = 'Сброс пароля'
+    template_name = os.path.join('password', 'reset-confirm.html')
+    success_url = reverse_lazy('user.password.reset.complete')
+
+
+class PasswordChangeView(auth_views.PasswordChangeView, DetailViewContextDefined):
+    pagename = 'Изменение пароля'
+    template_name = os.path.join('password', 'change-form.html')
+    success_url = 'index'
