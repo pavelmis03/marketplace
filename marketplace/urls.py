d2e00deeed47004c018fb22c30a6d51c4e9ac608
Daniil Konyagin <devdakone22@gmail.com>
2020-02-13 22:19:16 +0300

    Added media url

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from main import views as main_views
+from marketplace import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('user/', include('user.urls')),
 
     path('market/', include('market.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
