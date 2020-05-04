cff0e85ecb308099cfdc34393d5817ef3251d8ff
Andrew Smirnov <smirnov@informatics.ru>
2020-05-04 15:47:42 +0300

    Move from daphne to gunicorn due to old django version

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,8 @@
 cd /ms104_market/ || exit 1
 python manage.py migrate
 python manage.py collectstatic --noinput
-daphne -b 0.0.0.0 -p 8086 --access-log /var/log/ms104_market/daphne_access.log marketplace.asgi:application
+
+gunicorn marketplace.wsgi:application --bind 0.0.0.0:8086 \
+            --error-logfile /var/log/ms104_market/gunicorn_error.log \
+            --access-logfile /var/log/ms104_market/gunicorn_access.log \
+            --workers 2
