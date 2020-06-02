8447019b7b1584af7c2dd3547039702aa28edca4
Andrew Smirnov <smirnov@informatics.ru>
2020-06-02 21:39:32 +0300

    fix pycodestyle execution (remove venv)

--- ci/pycodestyle_runner.sh
+++ ci/pycodestyle_runner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pycodestyle . --statistics > public/pycodestyle.txt
+pycodestyle $(ls -d */ | grep -v -e static -e venv -e ci -e docs -e media -e public -e apps) --statistics > public/pycodestyle.txt
 exit 0
